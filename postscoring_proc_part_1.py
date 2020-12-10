f3adc3e28c2b305077d0532a7127b65b69c188a0
Sanjeev Gadre <sanjeev_gadre@hotmail.com>
2020-12-10 12:41:00 +0530

    Postscoring processing split into 2 parts for easy computing

---
+++ postscoring_proc_part_1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  9 09:43:17 2020
+
+@author: sanjeev
+"""
+
+#%% Libraries
+import numpy as np
+
+#%% Helper Variables
+inputpath = './data/'
+
+#%% Userscores pre-processing
+userscores = np.genfromtxt(inputpath + 'userscores.csv', delimiter = ',')
+
+# Identify indices of users not "processed" during scoring
+idx = np.where(userscores[:, 8] == 0)[0]
+# For unprocessed users, assign mean part scores
+userscores[idx, 1:8] = np.mean(userscores[:, 1:8], axis = 0)
+
+# Clip part userscores to within 2-sigma range for respective part scores
+mean = np.mean(userscores[:, 1:8], axis = 0)
+sigma = np.std(userscores[:, 1:8], axis = 0)
+two_sigma_range = two_sigma_range = np.concatenate(((mean - 2*sigma).reshape(-1, 1), 
+                                                    (mean + 2*sigma).reshape(-1, 1)), axis = 1)
+for i in range(7):
+    userscores[:, i + 1] = np.clip(userscores[:, i + 1], two_sigma_range[i, 0], two_sigma_range[i, 1])
+    
+# Write to disk
+np.savetxt(inputpath + 'userscores.csv', userscores, delimiter = ',')
+    
+#%% Questions pre-processing
+ques = np.genfromtxt(inputpath + 'ques.csv', delimiter = ',')
+
+# Indentify indices of questions not "processed" during scoring
+idx = np.where(ques[:, 2] == 0)[0]
+# For unprocessed questions, assign mean correct_attempt_prob for the part the questions belongs to
+for i in idx:
+    part = ques[i, 1]
+    ques[i, 3] = np.mean(ques[ques[:, 1] == part, 3])
+    
+# Write to disk
+np.savetxt(inputpath + 'ques.csv', ques, delimiter = ',')
+    
+
+
---
+++ postscoring_proc_part_2.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 10 10:04:28 2020
+
+@author: sanjeev
+"""
+
+#%% Libraries
+import numpy as np
+import pandas as pd
+import sys
+import datetime
+
+#%% Helper variables
+train_csv_path = '../input/riiid-test-answer-prediction/'
+other_csv_path = '../input/riiid-scoring/'
+# timeout for Kaggle processing
+time_out = 8.75 * 3600
+# records to read in a single batch
+chunksize_ = 10**6         
+# total records to read in a single run
+nrows_ = 12 * chunksize_
+# no. of rows to skip from top  
+skiprows_ = 1 + 1 * nrows_
+
+#%% Processing train.csv
+userscores = np.genfromtxt(other_csv_path + 'userscores.csv', delimiter = ',')
+ques = np.genfromtxt(other_csv_path + 'ques.csv', delimiter = ',')
+
+# column names for train.csv 
+colnames_ = ['row_id', 'timestamp', 'user_id', 'content_id', 'content_type_id', 
+             'task_container_id', 'user_answer', 'answered_correctly', 
+             'prior_question_elapsed_time', 'prior_question_had_explanation']
+
+reader = pd.read_csv(train_csv_path + 'train.csv', iterator = True, memory_map = True, names = colnames_, 
+                     skiprows = skiprows_, chunksize = chunksize_, nrows = nrows_)
+chunk_count = 0
+tic = datetime.datetime.now()
+
+for chunk in reader:
+    print('In the loop')
+    chunk_count += 1
+    # eliminating nans
+    chunk.loc[chunk['prior_question_had_explanation'].isna(), 'prior_question_had_explanation'] = False
+    chunk['prior_question_had_explanation'] = chunk['prior_question_had_explanation'].astype('int')
+    
+    # add columns for 7 part userscores, part# of the question and correct_attempt_prob for the question
+    chunk.loc[:, ['score_1', 'score_2', 'score_3', 'score_4', 'score_5', 
+                  'score_6', 'score_7', 'part', 'correct_attempt_prob']] = 0
+    colnames_ = chunk.columns
+    
+    # to_numpy
+    chunk = chunk.to_numpy()
+    
+    # identify records that are questions
+    qidx = np.where(chunk[:, 4] == 0)[0]
+    # new features are added only for records that are questions
+    for i in qidx:
+        chunk[i, 10:17] = userscores[np.where(userscores[:, 0] == chunk[i, 2])[0][0], 1:8]
+        chunk[i, 17:] = ques[np.where(ques[:, 0] == chunk[i, 3])[0], [1, 3]]
+            
+    # convert chunk to dataframe and then write to disk
+    chunk = pd.DataFrame(data = chunk, columns = colnames_)
+    chunk.to_hdf('train_scored.h5', key = 'df', mode = 'a', append = True, format = 'table')
+    
+    toc = datetime.datetime.now()
+    
+    # Am I running out of time on Kaggle?
+    if (toc-tic).total_seconds() >= time_out:
+        print('Pre-emptive exit to avoid timeout')
+        print('Skiprows: ', skiprows_, 'Chunk: ', chunk)
+        sys.exit()
+        
